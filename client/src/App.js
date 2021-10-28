61841d13ec5ebf0e084308d2fba69fe2e6fc1a1c
unknown <lautarorangilt@gmail.com>
2021-10-28 19:48:40 -0300

    landing

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,17 @@ import './App.css';
 import Home from './components/Home/Home';
 import NavBar from './components/NavBar/NavBar';
 import Cart from './components/Cart/Cart';
+import Landing from './components/Landing/Landing';
 
 function App() {
   return (
     <div className="App">
       {/* <header className="App-header">
       </header> */}
+       <Route exact path='/' component={Landing}/>
+      <Route exact path="/home" component={Home} />
       <Route exact path='/carrito' component={Cart} />
-      <Route path='/' component={NavBar} />
-      <Route exact path="/" component={Home} />
+      <Route path='/home' component={NavBar} />
     </div>
   );
 }
