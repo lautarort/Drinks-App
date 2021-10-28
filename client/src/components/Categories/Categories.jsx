746aa1109669c0dc91896888cdfc77bffd2fc727
Marco-Carballo-Francia <solojuegos.kiku2020@gmail.com>
2021-10-28 01:18:57 -0300

    Creacion del NavBar y estilos

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts, getCategories, filterByCategory } from '../../redux/actions/actions';
+import style from './Categories.module.css';
 
 const Categories = () => {
 
@@ -12,7 +13,7 @@ const Categories = () => {
 
 	const { categories } = useSelector(state => state);
 
-	function handleClick (e) {
+	function handleClick(e) {
 		if (!e.target.value) {
 			dispatch(getProducts());
 		}
@@ -22,15 +23,15 @@ const Categories = () => {
 	}
 
 	return (
-		<div>
-		<button value="" onClick={handleClick}>TODOS</button>
-		{
-			categories && categories.map(x => {
-				return (
-					<button value={x} onClick={handleClick}>{x.toUpperCase()}</button>
-				)
-			})
-		}
+		<div className={style.Categories}>
+			<button className={style.btn} value="" onClick={handleClick}>TODOS</button>
+			{
+				categories && categories.map(x => {
+					return (
+						<button className={style.btn} value={x} onClick={handleClick}>{x.toUpperCase()}</button>
+					)
+				})
+			}
 		</div>
 	)
 }
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Cards from '../Cards/Cards';
-import Categories from "../Categories/Categories";
 
 const Home = () => {
     return (
         <div>
             Desarrolladores Full-Scabio
-            <Categories />
             <Cards />
         </div>
     )
