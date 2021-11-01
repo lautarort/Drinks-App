c92de7b27e2e675df6c34f7e8e997364a5bf5354
David Heredia <davis_heredia@hotmail.com>
2021-11-01 19:31:28 -0300

    Arreglos

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import Cards from '../Cards/Cards';
 import Modal from '../Modal/Modal';
-import Pagination from "../Pagination/Pagination";
-import Pagination from "../Pagination/Pagination";  
+import Pagination from "../Pagination/Pagination"; 
 import CarouselCombo from "../CarouselCombo/CarouselCombo"
 
 const Home = () => {
