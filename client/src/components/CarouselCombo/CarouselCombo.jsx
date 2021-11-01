6d05b3283ae18905941fe1e77bd6758ba8091539
David Heredia <davis_heredia@hotmail.com>
2021-11-01 14:08:17 -0300

    cabiosCss

--- client/src/components/CarouselCombo/CarouselCombo.jsx
+++ client/src/components/CarouselCombo/CarouselCombo.jsx
@@ -19,7 +19,7 @@ const CarouselCombo = () => {
                 <Carousel.Caption className={style.ctntxt} >
                     <h1>Sabias que?</h1>
                     <p className={style.txt}>Tenemos muchos combos para que disfrutes justo a amigos</p>
-                    <Link to="/combos" ><button className={style.btn}>Entra</button> </Link>
+                    <Link to="/combos" ><button className={style.btn}>Entrar</button> </Link>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item className={style.ctn}>
@@ -32,7 +32,7 @@ const CarouselCombo = () => {
                 <Carousel.Caption className={style.ctntxt}>
                     <h1>Vinos tintos, blancos, rosados…?</h1>
                     <p className={style.txt}>Seleccione entre una gran variedad de vinos, de mucha calidad y algunos vinos que solo se elaboran en solo una parte del planeta</p>
-                    <button className={style.btn}>Entra </button>
+                    <button className={style.btn}>Entrar</button>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item className={style.ctn}>
@@ -44,8 +44,8 @@ const CarouselCombo = () => {
 
                 <Carousel.Caption className={style.ctntxt}>
                     <h1>Amante del Whisky? </h1>
-                    <p className={style.txt}>¿Nuca lo has probado y te quieres dar un capricho? No te preocupes… aquí podrás encontrar ese que tanto añoras.  </p>
-                    <Link to="/whisky"><button className={style.btn}>Entra </button> </Link>
+                    <p className={style.txt}>Nunca lo has probado y te quieres dar un capricho? No te preocupes… aquí podrás encontrar ese que tanto añoras.  </p>
+                    <Link to="/whisky"><button className={style.btn}>Entrar</button> </Link>
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
